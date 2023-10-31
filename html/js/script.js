e1c12391d6e75ac3c242de07797d3a85e3a838c0
Pedrocapacoca <141357297+Pedrocapacoca@users.noreply.github.com>
2023-10-31 11:25:35 -0300

    Add files via upload

---
+++ html/js/script.js
@@ -0,0 +1,28 @@
+// Função para pesquisar jogos
+function searchGames() {
+    const query = document.getElementById("search-input").value;
+    alert(`Pesquisando por: ${query}`);
+}
+// Função para alternar visibilidade do menu
+function toggleMenu() {
+    const menu = document.getElementById("side-menu");
+    if (menu.style.display === "none" || menu.style.display === "") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}
+//função para alterar a cor do site
+document.getElementById("modeSwitch").addEventListener("change", toggleMode);
+
+function toggleMode() {
+    const body = document.body;
+
+    if (body.classList.contains("light-mode")) {
+        body.classList.remove("light-mode");
+    } else {
+        body.classList.add("light-mode");
+    }
+}
+
+
